0899dc5ae70bb55c80dac1a5d30b99f17700be64
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-11 16:12:49 -0300

    V1.0 Home

--- src/javascript/menu_animations.js
+++ src/javascript/menu_animations.js
@@ -11,17 +11,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
   rotate();
 });
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  const starMove = document.getElementById("close_options_star");
+function abrirMenu() {
+  const logoMenu = document.getElementById("logo_menu");
+  const menuOptions = document.getElementById("menu_options");
 
-  function animateStar() {
-    const x = (Math.random() - 0.5) * 2;
-    const y = (Math.random() - 0.5) * 1;
+  // Adiciona a classe para a animação de opacidade
+  logoMenu.classList.add("fade-out");
 
-    starMove.style.transform = `translate(${x}px, ${y}px)`;
+  // Após a animação de opacidade, exibe as opções do menu com animação
+  setTimeout(() => {
+    logoMenu.style.display = "none";
+    menuOptions.style.display = "block";
+    menuOptions.classList.add("slide-in");
+  }, 1000); // Tempo igual ao da duração da animação CSS
+}
 
-    requestAnimationFrame(animateStar);
-  }
+function closeMenu() {
+  const menuOptions = document.getElementById("menu_options");
+  const logoMenu = document.getElementById("logo_menu");
 
-  animateStar();
-});
+  // Adiciona a classe para a animação de deslizamento e opacidade para fora
+  menuOptions.classList.remove("slide-in");
+  menuOptions.classList.add("slide-out");
+
+  // Após a animação de saída, esconde as opções do menu e mostra o logo com opacidade aumentando
+  setTimeout(() => {
+    menuOptions.style.display = "none";
+    logoMenu.style.display = "block";
+    logoMenu.classList.remove("fade-out");
+    logoMenu.classList.add("fade-in");
+  }, 1000); // Tempo igual ao da duração da animação CSS
+}
