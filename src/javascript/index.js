6d5b6e880d7c5dde99e8f1cffb4142485395ef64
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-14 16:57:40 -0300

    deu certo no texto da majesto, arruma o resto agora doidao

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,5 @@
-ScrollReveal({
-  distance: "20px",
-  delay: 400,
-  duration: 2500,
+ScrollReveal().reveal("#majesto_text", {
+  duration: 2000,
+  distance: "400px",
+  origin: "left",
 });
-
-ScrollReveal().reveal("#text_line");
