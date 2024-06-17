6b2fea398016de9588f90adaba7b5c2b3ccdde7f
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-17 17:00:11 -0300

    Fazer a função de rolar funcionar

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        console.log('Evento de rolagem acionado');
+        console.log($(this).scrollTop());
+    });
+});
+
+
+
+
 ScrollReveal().reveal("#majesto_text", {
   duration: 2000,
   distance: "400px",
