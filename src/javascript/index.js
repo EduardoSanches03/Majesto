e17049b89863c6fd1dfe41917f84736294ed26c1
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-19 15:28:00 -0300

    script para textos da pg3 feito

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,8 +1,21 @@
-const content = document.querySelector('.content')
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Verifica se o elemento tem a classe 'hiddenR'
+      if (entry.target.classList.contains("hiddenR")) {
+        entry.target.classList.add("showR");
+        // Verifica se o elemento tem a classe 'hidden'
+      } else if (entry.target.classList.contains("hidden")) {
+        entry.target.classList.add("show");
+      }
+    } else if (entry.target.classList.contains("showR")) {
+      entry.target.classList.remove("showR");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-const myObserver = new IntersectionObserver((entries) =>{
-    console.log(entries)
-})
+const elements = document.querySelectorAll(".hidden, .hiddenR");
 
-
-myObserver.observe(content)
+elements.forEach((element) => myObserver.observe(element));
