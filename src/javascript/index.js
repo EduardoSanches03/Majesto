752d63befdda27ce10ff27534ccd778339c8c3f6
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-19 16:23:03 -0300

    animação apliacada para PG2 + html alterado

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,21 +1,18 @@
 const myObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // Verifica se o elemento tem a classe 'hiddenR'
+      if (entry.target.classList.contains("hiddenU")) {
+        entry.target.classList.add("showU");
+      }
       if (entry.target.classList.contains("hiddenR")) {
         entry.target.classList.add("showR");
-        // Verifica se o elemento tem a classe 'hidden'
       } else if (entry.target.classList.contains("hidden")) {
         entry.target.classList.add("show");
       }
-    } else if (entry.target.classList.contains("showR")) {
-      entry.target.classList.remove("showR");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
 
-const elements = document.querySelectorAll(".hidden, .hiddenR");
+const elements = document.querySelectorAll(".hidden, .hiddenR, .hiddenU");
 
 elements.forEach((element) => myObserver.observe(element));
