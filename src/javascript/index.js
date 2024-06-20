758c09616548fda0a7c43370494ce41c0eb908fe
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-06-20 17:53:30 -0300

    falta so colocar o papel de fundo da sec4

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,18 +1,55 @@
-const myObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      if (entry.target.classList.contains("hiddenU")) {
-        entry.target.classList.add("showU");
-      }
-      if (entry.target.classList.contains("hiddenR")) {
-        entry.target.classList.add("showR");
-      } else if (entry.target.classList.contains("hidden")) {
-        entry.target.classList.add("show");
-      }
-    }
-  });
-});
+let rolagemTravada = false;
 
-const elements = document.querySelectorAll(".hidden, .hiddenR, .hiddenU");
+function travarRolagem() {
+  document.body.style.overflow = "hidden";
+  const hiddenIElement = document.querySelector(".hiddenI");
+  const hiddenIElement2 = document.querySelector(".text-container");
+  if (hiddenIElement) {
+    hiddenIElement.classList.add("showI");
+    setTimeout(() => {
+      hiddenIElement.classList.remove("showI");
+    }, 3000);
+  }
+  if (hiddenIElement2) {
+    hiddenIElement2.classList.add("showIn-Out-Up");
+    
+  }
+}
 
+function destravarRolagem() {
+  document.body.style.overflow = "";
+}
+
+const myObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains("hidden")) {
+          entry.target.classList.add("show");
+        }
+        if (entry.target.classList.contains("hiddenU")) {
+          entry.target.classList.add("showU");
+        }
+        if (entry.target.classList.contains("hiddenR")) {
+          entry.target.classList.add("showR");
+        }
+        if (
+          entry.target.classList.contains("scroll_pause") &&
+          !rolagemTravada
+        ) {
+          travarRolagem();
+          rolagemTravada = true; // Atualiza a flag para indicar que a rolagem foi travada
+          setTimeout(destravarRolagem, 3000);
+        }
+      }
+    });
+  },
+);
+
+// Seleciona todos os elementos que queremos observar
+const elements = document.querySelectorAll(
+  ".hidden, .hiddenI, .hiddenR, .hiddenU, .scroll_pause, .hiddenIn-Out-Up"
+);
+
+// Adiciona os elementos ao observer
 elements.forEach((element) => myObserver.observe(element));
