de33124de6e6e9a9976bc3f49e76a72be58a16ad
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2024-11-05 12:06:27 -0300

    Menu mobile

--- src/javascript/menu_animations.js
+++ src/javascript/menu_animations.js
@@ -11,51 +11,50 @@ document.addEventListener("DOMContentLoaded", (event) => {
   rotate();
 });
 
-
+// Função para alternar o menu
 function toggleMenu() {
   const logoMenu = document.getElementById("logo_menu");
   const menuMobile = document.getElementById("menu-mobile");
   const menuOptions = document.getElementById("menu_options");
+  const menuModal = document.getElementById("menuModal");
 
-  // Verifica se o menu está visível
-  const isOpen = menuOptions.style.display === "block";
+  if (window.innerWidth > 768) {
+    const isOpen = menuOptions.style.display === "block";
 
-  if (isOpen) {
-    // Fecha o menu para desktop e mobile
-    menuOptions.classList.remove("slide-in");
-    menuOptions.classList.add("slide-out");
+    if (isOpen) {
+      menuOptions.classList.remove("slide-in");
+      menuOptions.classList.add("slide-out");
 
-    setTimeout(() => {
-      menuOptions.style.display = "none";
-      menuOptions.classList.remove("slide-out");
-
-      if (window.innerWidth > 768) {
+      setTimeout(() => {
+        logoMenu.classList.add("fade-out");
+        menuOptions.style.display = "none";
+        menuOptions.classList.remove("slide-out");
         logoMenu.style.display = "block";
-        logoMenu.classList.remove("fade-out");
-        logoMenu.classList.add("fade-in");
-      } else {
-        menuMobile.style.display = "block";
-        menuMobile.classList.remove("fade-out");
-        menuMobile.classList.add("fade-in");
-      }
-    }, 1000); // Ajuste de acordo com a duração da animação CSS
-  } else {
-    // Abre o menu para desktop e mobile
-    if (window.innerWidth > 768) {
-      logoMenu.classList.add("fade-out");
+      }, 1000);
     } else {
-      menuMobile.classList.add("fade-out");
-    }
-
-    setTimeout(() => {
-      if (window.innerWidth > 768) {
-        logoMenu.style.display = "none";
-      } else {
-        menuMobile.style.display = "none";
-      }
-
+      // Abre o menu para desktop
+      logoMenu.style.display = "none"; // Esconde o logo
+      logoMenu.classList.add("fade-in");
       menuOptions.style.display = "block";
       menuOptions.classList.add("slide-in");
-    }, 1000); // Ajuste de acordo com a duração da animação CSS
+    }
+  } else {
+    // Modo mobile: alterna a exibição da modal
+    const isModalOpen = menuModal.style.display === "flex";
+
+    if (isModalOpen) {
+      // Fecha o menu modal
+      menuModal.classList.remove("fade-in");
+      menuModal.classList.add("fade-out");
+
+      setTimeout(() => {
+        menuModal.style.display = "none"; // Esconde a modal
+        menuModal.classList.remove("fade-out");
+      }, 1000); // Ajuste de acordo com a duração da animação CSS
+    } else {
+      // Abre o menu modal
+      menuModal.style.display = "flex";
+      menuModal.classList.add("fade-in");
+    }
   }
 }
